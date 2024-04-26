e94d55956ff97cd4855f7356848a917e700c52f5
rilesSmiles <riley.schmitz@edu.sait.ca>
2024-04-26 02:08:56 -0600

    accordion fixed?

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
             document.addEventListener("DOMContentLoaded", function() {
-                const accordionButtons = document.querySelectorAll('.accordion-button');
+                const accordionButtons = document.querySelectorAll('accordion-button');
             
                 accordionButtons.forEach(button => {
                     button.addEventListener('click', function() {
                         const accordionItem = this.parentNode;
                         accordionItem.classList.toggle('active');
-                        
-            const otherAccordionItems = document.querySelectorAll('.accordion-item');
+
+            const otherAccordionItems = document.querySelectorAll('accordion-item');
             otherAccordionItems.forEach(item => {
                 if (item !== accordionItem) {
                     item.classList.remove('active');
