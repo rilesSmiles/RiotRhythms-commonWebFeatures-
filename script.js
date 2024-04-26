b66f19d0d994d0501acd4188df0d302e0acf4df3
rilesSmiles <riley.schmitz@edu.sait.ca>
2024-04-26 02:03:31 -0600

    accordion created

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const accordionItems = document.querySelectorAll('.accordion-item');
 
-    accordionItems.forEach(item => {
-        const button = item.querySelector('.accordion-button');
-
-        button.addEventListener('click', function() {
-            item.classList.toggle('active');
-
-            accordionItems.forEach(otherItem => {
-                if (otherItem !== item) {
-                    otherItem.classList.remove('active');
+            document.addEventListener("DOMContentLoaded", function() {
+                const accordionButtons = document.querySelectorAll('.accordion-button');
+            
+                accordionButtons.forEach(button => {
+                    button.addEventListener('click', function() {
+                        const accordionItem = this.parentNode;
+                        accordionItem.classList.toggle('active');
+                        
+            const otherAccordionItems = document.querySelectorAll('.accordion-item');
+            otherAccordionItems.forEach(item => {
+                if (item !== accordionItem) {
+                    item.classList.remove('active');
+                    item.querySelector('.accordion-content').style.display = 'none';
                 }
             });
         });
     });
 });
+
